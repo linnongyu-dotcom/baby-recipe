20dc48d45ac50ea319f90f2592763ee81d894525
linnongyu-dotcom <linnongyu@gmail.com>
2026-08-04 22:52:29 +0800

    fix: move cache bootstrap out of conflicted modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { invalidateBrowserMealPlan } from './utils/mealPlanCache'
+import App from './App'
 import './index.css'
 
-async function startApp() {
-  // App imports useStore. Load it only after localStorage has been migrated so
-  // Zustand cannot hydrate the stale plan and later write it back over the fix.
-  invalidateBrowserMealPlan()
-  const { default: App } = await import('./App')
-
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
-}
-
-void startApp()
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
